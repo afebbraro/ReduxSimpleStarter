8fb5caba0213c1e6a32e2eb148e85db5c616ffa5
Amber Febbraro <amberfebbraro@quickenloans.com>
2018-05-30 17:18:51 -0400

    lecture 29

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search-bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 
 const appContainer = document.querySelector('.container');
 
-// Create a new component. This component should produce some HTML.
-// This is a class of a component
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'trail running'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 };
 
 // Take this component's generated HTML and put it in the DOM.
